8e9a065ff82dc30ba8710ba1c8200059523d892d
Боил Василев <boil4o@abv.bg>
2025-01-27 21:06:11 +0200

    Connected MySql database

--- HotelReservation/Data/ApplicationDbContext.cs
+++ HotelReservation/Data/ApplicationDbContext.cs
@@ -5,5 +5,16 @@ namespace HotelReservation.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Base configuration is usually done in Program.cs
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySql(
+                    "YourConnectionStringHere",
+                    ServerVersion.AutoDetect("YourConnectionStringHere")
+                );
+            }
+        }
     }
 }
